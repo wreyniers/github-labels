bb51692dbf8e97a7f7b52209b7496dbf84755b2a
Wouter Reyniers <wouter@bythepixel.com>
2018-10-30 11:26:30 -0600

    Get github labels

---
+++ get-github-labels.js
@@ -0,0 +1,17 @@
+var labels = [];
+[].slice.call(document.querySelectorAll(".label-link"))
+.forEach(function(element) {
+  labels.push({
+    name: element.textContent.trim(),
+    // using style.backgroundColor might returns "rgb(...)"
+    color: element.getAttribute("style")
+      .replace("background-color:", "")
+      .replace(/color:.*/,"")
+      .trim()
+      // github wants hex code only without # or ;
+      .replace(/^#/, "")
+      .replace(/;$/, "")
+      .trim(),
+  })
+})
+console.log(JSON.stringify(labels, null, 2))
